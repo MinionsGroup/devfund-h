b6a4b4d49a297fabc46548de03220929ba749897
Miguel C <miguel.calderon@communitybrands.com>
2018-07-05 22:04:34 -0400

    Kata-CountCharacters solved

---
+++ src/main/java/org/minions/devfund/CountCharacters.java
@@ -0,0 +1,35 @@
+package org.minions.devfund;
+
+import java.util.*;
+
+/**
+ * Solution for "count the characters" kata.
+ *
+ * @version 1.0 18 Jul 2018  * @author
+ * Miguel Calderon
+ */
+
+public class CountCharacters {
+
+    /**
+     * Private constructor.
+     */
+    private CountCharacters() {
+    }
+
+    /**
+     * Counts the how many times char "e" appears in word.
+     */
+    public static void main(final String[] args) {
+        String word = "Martes de fuchitos";
+        char character = 'e';
+        int result = 0;
+
+        for (char c : word.toCharArray()) {
+            if (c == character) {
+                result++;
+            }
+        }
+        System.out.print(result);
+    }
+}
