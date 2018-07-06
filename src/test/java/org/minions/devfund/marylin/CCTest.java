be39fa4c0ffc6e6deade30b76246197f26f25ef1
Marylin Moya <marylin.moya.jala@gmail.com>
2018-07-06 18:41:05 -0400

    change package name

--- src/test/java/org/minions/devfund/marylin/CCTest.java
+++ src/test/java/org/minions/devfund/marylin/CCTest.java
@@ -1,7 +1,6 @@
-package org.minions.devfund.Marylin;
+package org.minions.devfund.marylin;
 
 import org.junit.Test;
-import org.minions.devfund.marylin.CC;
 
 import static org.junit.Assert.assertEquals;
 
