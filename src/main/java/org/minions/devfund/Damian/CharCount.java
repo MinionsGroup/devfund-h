8aa624451d3a645b8496981a13850f363d6383b0
Damian Villanueva <penieldvp18@gmail.com>
2018-07-07 02:15:08 -0400

    Submited Kata #1 related to count of characters in a string

---
+++ src/main/java/org/minions/devfund/Damian/CharCount.java
@@ -0,0 +1,22 @@
+package org.minions.devfund.Damian;
+
+import java.util.regex.Pattern;
+
+/**
+ * Class solution for Kata #1: Count the Characters.
+ *
+ * @author Damian
+ */
+
+public class CharCount {
+    /**
+     * method to count the number of times that a characters appears in a string.
+     *
+     * @param str String type
+     * @param c   Sting type
+     * @return the number of times that character appears in the string (insensitive case)
+     */
+    public int characterCounter(final String str, final char c) {
+        return Pattern.compile("(?i)[^" + c + "]").matcher(str).replaceAll("").length();
+    }
+}
