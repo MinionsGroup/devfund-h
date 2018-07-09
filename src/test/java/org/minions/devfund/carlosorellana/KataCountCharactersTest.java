d099d93483be06f6eefa41a755c8d9bf5448436e
Carlos <carlos.orellanacanedo@outlook.com>
2018-07-09 14:19:11 -0400

    Apply comment in the practice.

--- src/test/java/org/minions/devfund/carlosorellana/KataCountCharactersTest.java
+++ src/test/java/org/minions/devfund/carlosorellana/KataCountCharactersTest.java
@@ -1,8 +1,9 @@
 package org.minions.devfund.carlosorellana;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * The goal is test the count characters methods.
@@ -13,8 +14,6 @@ import org.junit.Test;
 public class KataCountCharactersTest {
 
     private KataCountCharacters kataCountCharacters;
-    private static final int NUMBER_TWO = 2;
-    private static final int NUMBER_THIRTY_THREE = 33;
 
     /**
      * Set ups method.
@@ -29,8 +28,9 @@ public class KataCountCharactersTest {
      */
     @Test
     public void countCharacteresElephantSentence() {
+        final int expectedResult = 2;
         int numberCharacters = this.kataCountCharacters.countCharacteres("elephant", 'e');
-        Assert.assertEquals(NUMBER_TWO, numberCharacters);
+        assertEquals(expectedResult, numberCharacters);
     }
 
     /**
@@ -38,9 +38,20 @@ public class KataCountCharactersTest {
      */
     @Test
     public void countCharacteresParagraphSentence() {
+        final int expectedResult = 36;
         String paragraph = "Set of features, qualities or circumstances that indicate the nature of a thing or the way "
                 + "of thinking and acting of a person or a community, and by which it is distinguished from others.";
         int numberCharacters = this.kataCountCharacters.countCharacteres(paragraph, ' ');
-        Assert.assertEquals(NUMBER_THIRTY_THREE, numberCharacters);
+        assertEquals(expectedResult, numberCharacters);
+    }
+
+    /**
+     * Runs test case countCharacteresMayusSentence.
+     */
+    @Test
+    public void countCharacteresSentence() {
+        final int expectedResult = 4;
+        int numberCharacters = this.kataCountCharacters.countCharacteres("fizzbuzz", 'Z');
+        assertEquals(expectedResult, numberCharacters);
     }
 }
