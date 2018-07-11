2021be06cadd930039c72f3d9519b04b70e46553
amilcarmaida <amilcar.maida@gmail.com>
2018-07-11 06:55:29 -0700

    fixing issues about static final variables

--- src/test/java/org/minions/devfund/amilcar/CountRepeatedCharsTest.java
+++ src/test/java/org/minions/devfund/amilcar/CountRepeatedCharsTest.java
@@ -11,14 +11,10 @@ public class CountRepeatedCharsTest {
      * Unit test method to valida the expected amount of characters.
      */
     @Test
-<<<<<<< HEAD
     public void testCountRepeatedChars() {
-=======
-    public void testCountRepeatedCharacters() {
->>>>>>> e0c6fc1035d9b96c4b1f29869d4d3acd596949bd
-        Assert.assertEquals(CountRepeatedChars.FOUR, CountRepeatedChars.charCount("fizzbuzz", 'z'));
-        Assert.assertEquals(CountRepeatedChars.FOUR, CountRepeatedChars.charCount("FIZZBUZZ", 'z'));
-        Assert.assertEquals(CountRepeatedChars.EIGHT, CountRepeatedChars.charCount("fizzbuzz FIZZBUZZ", 'z'));
-        Assert.assertNotEquals(CountRepeatedChars.TWO, CountRepeatedChars.charCount("fizzbuzz FIZZBUZZ", 'z'));
+            Assert.assertEquals(CountRepeatedChars.FOUR, CountRepeatedChars.charCount("fizzbuzz", 'z'));
+            Assert.assertEquals(CountRepeatedChars.FOUR, CountRepeatedChars.charCount("FIZZBUZZ", 'z'));
+            Assert.assertEquals(CountRepeatedChars.EIGHT, CountRepeatedChars.charCount("fizzbuzz FIZZBUZZ", 'z'));
+            Assert.assertNotEquals(CountRepeatedChars.TWO, CountRepeatedChars.charCount("fizzbuzz FIZZBUZZ", 'z'));
     }
 }
