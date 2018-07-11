9d1986aa00afbf652cf207bd49776a10f0a038c5
Abel Barrientos <abelbs27101@gmail.com>
2018-07-10 22:51:22 -0400

    Refactored to the suggested comment

--- src/main/java/org/minions/devfund/abel/CountCharacters.java
+++ src/main/java/org/minions/devfund/abel/CountCharacters.java
@@ -1,6 +1,5 @@
 package org.minions.devfund.abel;
 
-import java.util.Arrays;
 
 /**
  * Solving Kata https://www.codewars.com/kata/count-the-characters/java .
@@ -10,17 +9,19 @@ public final class CountCharacters {
     /**
      * Private Constructor.
      */
-    private CountCharacters() { }
+    private CountCharacters() {
+    }
 
     /**
      * Counts the repetitions of an specific character in a String.
+     *
      * @param str The string to review.
-     * @param c The character to check repetitions.
+     * @param c   The character to check repetitions.
      * @return int type.
      */
-    public static int charCount(final String str, final char c) {
-        return (int) Arrays.stream(str.split(""))
-                .filter(ch -> ch.equalsIgnoreCase(Character.toString(c)))
+    public static int charCount(final String str, char c) {
+        return (int) str.toLowerCase().chars()
+                .filter(ch -> ch == Character.toLowerCase(c))
                 .count();
     }
 }
