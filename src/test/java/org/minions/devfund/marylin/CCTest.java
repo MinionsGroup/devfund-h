9364ddb9f63fe5db12ad04859f9b3dff707d7e96
Marylin Moya <marylin.moya.jala@gmail.com>
2018-07-12 21:18:10 -0400

    fix code review observations

--- src/test/java/org/minions/devfund/marylin/CCTest.java
+++ src/test/java/org/minions/devfund/marylin/CCTest.java
@@ -1,5 +1,6 @@
 package org.minions.devfund.marylin;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,13 +9,21 @@ import static org.junit.Assert.assertEquals;
  * Class to run unit test against CC.java class.
  */
 public class CCTest {
+    private CC cc;
+
+    /**
+     * Method to create an instance of CC class.
+     */
+    @Before
+    public void setUp() {
+        cc = new CC();
+    }
 
     /**
      * Method to count all the lower case characters.
      */
     @Test
     public void testLowerCaseCharacters() {
-        CC cc = new CC();
         final int expectedResult = 4;
         assertEquals(expectedResult, cc.charCount("fizzbuzz", 'z'));
 
@@ -25,7 +34,6 @@ public class CCTest {
      */
     @Test
     public void testUpperCaseCharacters() {
-        CC cc = new CC();
         final int expectedResult = 4;
         assertEquals(expectedResult, cc.charCount("FIZZBUZZ", 'z'));
     }
@@ -35,7 +43,6 @@ public class CCTest {
      */
     @Test
     public void testLowerAndUpperCaseCharacters() {
-        CC cc = new CC();
         final int expectedResult = 5;
         assertEquals(expectedResult, cc.charCount("fizZbuzZz", 'z'));
     }
