b659226ccb4a0511a29f0132cb84893a7c7ada65
Marylin Moya <marylin.moya.jala@gmail.com>
2018-07-13 15:26:25 -0400

    remove main method

--- src/main/java/org/minions/devfund/marylin/WhackAMole.java
+++ src/main/java/org/minions/devfund/marylin/WhackAMole.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 /**
  * Class to simulate whack a mole.
  */
-public final class WhackAMole {
+public class WhackAMole {
     private static final Logger LOGGER = Logger.getLogger(WhackAMole.class.getName());
     private int score;
     private int molesLeft;
@@ -22,7 +22,7 @@ public final class WhackAMole {
      * @param numAttempts   Number of the attempts.
      * @param gridDimension Number of the grid dimension.
      */
-    private WhackAMole(int numAttempts, int gridDimension) {
+    public WhackAMole(int numAttempts, int gridDimension) {
         this.moleGrid = new char[gridDimension][gridDimension];
         this.attemptsLeft = numAttempts;
 
@@ -38,7 +38,7 @@ public final class WhackAMole {
      * @param y Y axis in the grid.
      * @return Returns true if the mole was place, false otherwise.
      */
-    private boolean place(int x, int y) {
+    public boolean place(int x, int y) {
         if (this.moleGrid[x][y] != 'M') {
             this.moleGrid[x][y] = 'M';
             this.molesLeft++;
@@ -53,7 +53,7 @@ public final class WhackAMole {
      * @param x X axis in the grid.
      * @param y Y axis in the grid.
      */
-    private void whack(int x, int y) {
+    public void whack(int x, int y) {
         if (this.moleGrid[x][y] == 'M') {
             this.moleGrid[x][y] = 'W';
             this.score++;
@@ -65,7 +65,7 @@ public final class WhackAMole {
     /**
      * Method to print the status of the game without shows the moles.
      */
-    private void printGridToUser() {
+    public void printGridToUser() {
         for (char[] rowGrid : this.moleGrid) {
             StringBuilder strBuilderRow = new StringBuilder();
             for (char charItem : rowGrid) {
@@ -84,7 +84,7 @@ public final class WhackAMole {
     /**
      * Method to print the grid showing the status of the game even moles.
      */
-    private void printGrid() {
+    public void printGrid() {
         for (char[] rowGrid : this.moleGrid) {
             StringBuilder strBuilderRow = new StringBuilder();
             for (char charItem : rowGrid) {
@@ -92,84 +92,6 @@ public final class WhackAMole {
                 strBuilderRow.append("  ");
             }
             LOGGER.fine(strBuilderRow.toString());
-        }
-    }
-
-    /**
-     * Main method to play the game.
-     *
-     * @param args Default Argument.
-     */
-    public static void main(final String[] args) {
-        int numberOfMoles = 10;
-        int numAttemps = 50;
-        int gridLenght = 10;
-
-        //create a 10 by 10 grid
-        WhackAMole whackAMole = new WhackAMole(numAttemps, gridLenght);
-
-        //Place randomly a total of 10 moles
-        for (int i = 0; i < numberOfMoles; i++) {
-            Random rand = new Random();
-            int x = rand.nextInt(numberOfMoles);
-            int y = rand.nextInt(numberOfMoles);
-            StringBuilder strBuilderMessage = new StringBuilder();
-            if (!whackAMole.place(x, y)) {
-                strBuilderMessage.append("There is already a mole at ");
-                strBuilderMessage.append(x);
-                strBuilderMessage.append(",");
-                strBuilderMessage.append(y);
-                strBuilderMessage.append(". Trying again!");
-                i--;
-            } else {
-                strBuilderMessage.append("Mole places at ");
-                strBuilderMessage.append(x);
-                strBuilderMessage.append(",");
-                strBuilderMessage.append(y);
-            }
-            LOGGER.fine(strBuilderMessage.toString());
-        }
-
-        //Try to whack 50 times the moles given the x and y
-        //Finish the game if we enter xAxis -1 and yAxis -1 printing the grid
-        //The game ends if the user is able to uncover all the moles or if the user runs out of attempts.
-
-        LOGGER.info("-------------------------------------");
-        whackAMole.printGrid();
-        LOGGER.log(Level.INFO, "-----You have a maximum of {0} attempts to get all the moles wacked-----", numAttemps);
-
-        int xAxis;
-        int yAxis;
-        Scanner xScanner;
-        Scanner yScanner;
-        for (int i = numAttemps; i > 0; i--) {
-            xScanner = new Scanner(System.in);
-            LOGGER.fine("Enter the x coordinates: ");
-            xAxis = xScanner.nextInt();
-
-            yScanner = new Scanner(System.in);
-            LOGGER.fine("Enter the y coordinates: ");
-            yAxis = yScanner.nextInt();
-
-            if (xAxis == -1 || yAxis == -1) {
-                LOGGER.fine("Giving up!!");
-                whackAMole.printGrid();
-                break;
-            } else {
-                LOGGER.fine("State of Game");
-                whackAMole.whack(xAxis, yAxis);
-                whackAMole.printGridToUser();
-            }
-
-            if (whackAMole.molesLeft == 0) {
-                LOGGER.log(Level.INFO, "All moles were whacked with score: {0}", whackAMole.score);
-                break;
-            }
-
-            if (whackAMole.attemptsLeft == 0) {
-                LOGGER.log(Level.INFO, "There are no more attemps with score: {0}", whackAMole.score);
-                break;
-            }
         }
     }
 }
