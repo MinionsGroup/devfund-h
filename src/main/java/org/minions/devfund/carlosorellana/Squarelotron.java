93a9ef5ba878ededadfa5aba7db8f727fa94e23e
Carlos <carlos.orellanacanedo@outlook.com>
2018-07-17 19:26:42 -0400

    Squarelotron: first commint

---
+++ src/main/java/org/minions/devfund/carlosorellana/Squarelotron.java
@@ -0,0 +1,17 @@
+package org.minions.devfund.carlosorellana;
+
+/**
+ * The goal is count the number of characters appears in a string.
+ *
+ * @author Carlos Orellana.
+ * @since 07/06/2018.
+ */
+public class Squarelotron {
+
+    private int[][] squarelotron;
+    private int size;
+
+    public Squarelotron(int n) {
+
+    }
+}
