ee202dfc71f7f2115b2d61676ac1dc469bd6dae2
roy_rodriguez <Roy.Rodriguez@idt.net>
2018-07-26 23:01:56 -0400

    Squarelotron update

--- src/main/java/org/minions/devfund/royrodriguez/Squarelotron.java
+++ src/main/java/org/minions/devfund/royrodriguez/Squarelotron.java
@@ -2,25 +2,51 @@ package org.minions.devfund.royrodriguez;
 
 import java.util.Arrays;
 
+/**
+ * Squarelotron class.
+ */
 public class Squarelotron {
-    public int[][] squarelotron;
-    int size;
+    private final int[][] squarelotron;
+    private int size;
 
+    /**
+     * Squarelotron constructor.
+     *
+     * @param n matrix's size.
+     */
     public Squarelotron(int n) {
         this.size = n;
         this.squarelotron = new int[n][n];
         initialize(squarelotron);
     }
 
-    public Squarelotron(int[][] matrix, int size) {
+    /**
+     * Gets squarelotron matrix.
+     *
+     * @return matrix.
+     */
+    public int[][] getSquarelotron() {
+        return deepCopy(squarelotron);
+    }
+
+    /**
+     * Squarelotron constructor to replace matrix.
+     *
+     * @param matrix new matrix.
+     * @param size   size of the new matrix.
+     */
+    public Squarelotron(final int[][] matrix, int size) {
         this.size = size;
         this.squarelotron = deepCopy(matrix);
     }
 
+    /**
+     * Deep copy of the matrix.
+     *
+     * @param original original matrix.
+     * @return new matrix.
+     */
     private int[][] deepCopy(final int[][] original) {
-        if (original == null) {
-            return new int[][]{};
-        }
         final int[][] result = new int[original.length][];
         for (int i = 0; i < original.length; i++) {
             result[i] = Arrays.copyOf(original[i], original[i].length);
@@ -28,7 +54,12 @@ public class Squarelotron {
         return result;
     }
 
-    private void initialize(int[][] matrix) {
+    /**
+     * Initializes the squarelotron matrix.
+     *
+     * @param matrix matrix to initialize.
+     */
+    private void initialize(final int[][] matrix) {
         int value = 1;
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix.length; col++) {
@@ -38,23 +69,52 @@ public class Squarelotron {
         }
     }
 
+    /**
+     * Determines if the position belongs to the ring.
+     *
+     * @param ring number of the ring.
+     * @param x    position.
+     * @param y    position.
+     * @return true if the position belongs to the ring, false otherwise.
+     */
     public boolean belongsToTheRing(int ring, int x, int y) {
-        if (x < size + 1 && y < size + 1 && ring > 0) {
-            if (!belongsToTheRing(ring - 1, x, y)) {
-                if (((x == ring || y == ring))) {
-                    return true;
-                }
-                if (x == ring + size - odd(ring)) return true;
-                if (y == ring + size - odd(ring)) return true;
-            }
+        if (belongsToTheMatrix(ring, x, y)
+                && !belongsToTheRing(ring - 1, x, y)
+                && (x == ring
+                || y == ring
+                || y == ring + size - odd(ring))) {
+            return true;
         }
         return false;
     }
 
+    /**
+     * Verifies if the values belongs the matrix.
+     * @param ring number.
+     * @param x position.
+     * @param y position.
+     * @return true if belongs the matrix.
+     */
+    public boolean belongsToTheMatrix(int ring, int x, int y) {
+        return x < size + 1 && y < size + 1 && ring > 0;
+    }
+
+    /**
+     * Determines the n odd.
+     *
+     * @param n number of odd.
+     * @return the n-odd.
+     */
     public int odd(int n) {
         return 2 * n - 1;
     }
 
+    /**
+     * Flips rows of a ring.
+     *
+     * @param ring number of the ring
+     * @return new Squarelotron with the result as a matrix.
+     */
     public Squarelotron upsideDownFlip(int ring) {
         int[][] matrix = deepCopy(squarelotron);
         for (int row = 0; row < matrix.length / 2; row++) {
@@ -64,29 +124,31 @@ public class Squarelotron {
                 }
             }
         }
-        return new Squarelotron(matrix, size);
+        return new Squarelotron(matrix, matrix.length);
     }
 
-    private void flip(int row, int col, int[][] matrix, int matrixSize) {
+    /**
+     * Flips values.
+     *
+     * @param row        number of row.
+     * @param col        number of column.
+     * @param matrix     matrix to flip.
+     * @param matrixSize size of the matrix.
+     */
+    private void flip(int row, int col, final int[][] matrix, int matrixSize) {
         int fRow = row + matrixSize - odd(row) - 2;
         int aux = matrix[row][col];
         matrix[row][col] = matrix[fRow][col];
         matrix[fRow][col] = aux;
     }
 
-    public void printSquarelotron() {
-        for (int row = 0; row < squarelotron.length; row++) {
-            for (int col = 0; col < squarelotron.length; col++) {
-                System.out.print(squarelotron[row][col] + "\t");
-            }
-            System.out.println();
-        }
-    }
-
-    public Squarelotron mainDiagonalFlip(int ring) throws NumberFormatException {
-        if (ring > maxRings(size) || ring < 1) {
-            throw new NumberFormatException();
-        }
+    /**
+     * Flips around a main diagonal.
+     *
+     * @param ring number of the ring.
+     * @return Squarelotron with the result as its matrix.
+     */
+    public Squarelotron mainDiagonalFlip(int ring) {
         int[][] matrix = deepCopy(squarelotron);
         int aux;
         for (int col = ring - 1; col <= matrix.length - ring; col++) {
@@ -105,9 +167,15 @@ public class Squarelotron {
         return new Squarelotron(matrix, matrix.length);
     }
 
+    /**
+     * Rotates the matrix to the right.
+     *
+     * @param numberOfTurns number of rotations.
+     */
     public void rotateRight(int numberOfTurns) {
+        final int sides = 4;
         if (numberOfTurns < 0) {
-            numberOfTurns = (numberOfTurns / 4) > 0 ? numberOfTurns / 4 + 4 : numberOfTurns + 4;
+            numberOfTurns = (numberOfTurns / sides) < -1 ? numberOfTurns % sides + sides : numberOfTurns + sides;
         }
         while (numberOfTurns > 0) {
             int[][] copy = deepCopy(squarelotron);
@@ -126,6 +194,12 @@ public class Squarelotron {
         }
     }
 
+    /**
+     * Returns the max rings for a matrix.
+     *
+     * @param size of the matrix.
+     * @return max possible rings.
+     */
     private int maxRings(int size) {
         return size % 2 == 0 ? size / 2 : (size / 2) + 1;
     }
--- src/test/java/org/minions/devfund/royrodriguez/SquarelotronTest.java
+++ src/test/java/org/minions/devfund/royrodriguez/SquarelotronTest.java
@@ -3,7 +3,18 @@ package org.minions.devfund.royrodriguez;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * Squearelotron tests.
+ */
 public class SquarelotronTest {
+    private static final int FOUR = 4;
+    private static final int THREE = 3;
+    private static final int FIVE = 5;
+    private static final int EIGHT = 8;
+
+    /**
+     * Test upside down flip dimension 4 ring 1.
+     */
     @Test
     public void upsideDownFlipTest() {
         final int[][] expectedResult = {
@@ -12,10 +23,13 @@ public class SquarelotronTest {
                 {5, 10, 11, 8},
                 {1, 2, 3, 4}
         };
-        Squarelotron squarelotron = new Squarelotron(4);
-        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(1).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FOUR);
+        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(1).getSquarelotron());
     }
 
+    /**
+     * Tests upSideDownFlip method dimension 4 ring 2.
+     */
     @Test
     public void testUpsideDownFlipDimensionFour() {
         final int[][] expectedResult = {
@@ -24,10 +38,13 @@ public class SquarelotronTest {
                 {9, 6, 7, 12},
                 {13, 14, 15, 16}
         };
-        Squarelotron squarelotron = new Squarelotron(4);
-        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(2).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FOUR);
+        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(2).getSquarelotron());
     }
 
+    /**
+     * Tests upSideDownFlipMethod dimension three ring 1.
+     */
     @Test
     public void testUpsideDownFlipDimensionThreeRingOne() {
         final int[][] expectedResult = {
@@ -35,10 +52,13 @@ public class SquarelotronTest {
                 {4, 5, 6},
                 {1, 2, 3}
         };
-        Squarelotron squarelotron = new Squarelotron(3);
-        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(1).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(THREE);
+        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(1).getSquarelotron());
     }
 
+    /**
+     * Tests upSideDownFlip method dimension three ring two.
+     */
     @Test
     public void testUpsideDownFlipDimensionThreeRingTwo() {
         final int[][] expectedResult = {
@@ -46,10 +66,13 @@ public class SquarelotronTest {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        Squarelotron squarelotron = new Squarelotron(3);
-        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(2).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(THREE);
+        Assert.assertEquals(expectedResult, squarelotron.upsideDownFlip(2).getSquarelotron());
     }
 
+    /**
+     * Tests mainDiagonalFlip method dimension 4 ring 1.
+     */
     @Test
     public void mainDiagonalFlipTest() {
         final int[][] expectedResult = {
@@ -59,10 +82,13 @@ public class SquarelotronTest {
                 {4, 8, 12, 16}
         };
 
-        Squarelotron squarelotron = new Squarelotron(4);
-        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(1).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FOUR);
+        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(1).getSquarelotron());
     }
 
+    /**
+     * Tests mainDiagonalFlip method dimension 4 ring 2.
+     */
     @Test
     public void mainDiagonalFlipTestSecondRing() {
         final int[][] expectedResult = {
@@ -72,10 +98,13 @@ public class SquarelotronTest {
                 {13, 14, 15, 16}
         };
 
-        Squarelotron squarelotron = new Squarelotron(4);
-        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(2).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FOUR);
+        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(2).getSquarelotron());
     }
 
+    /**
+     * Tests mainDiagonalFlip method dimension 5 ring 3.
+     */
     @Test
     public void testMainDiagonalFlipDimensionFiveRingThree() {
         final int[][] expectedResult = {
@@ -86,48 +115,60 @@ public class SquarelotronTest {
                 {21, 22, 23, 24, 25}
         };
 
-        Squarelotron squarelotron = new Squarelotron(5);
-        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(3).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FIVE);
+        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(THREE).getSquarelotron());
     }
 
+    /**
+     * Tests rotateRing method dimension 4 rotates 1.
+     */
     @Test
     public void rotateRingTest() {
-        int[][] expectedResult = {
+        final int[][] expectedResult = {
                 {13, 9, 5, 1},
                 {14, 10, 6, 2},
                 {15, 11, 7, 3},
                 {16, 12, 8, 4}
         };
-        Squarelotron squarelotron = new Squarelotron(4);
+        Squarelotron squarelotron = new Squarelotron(FOUR);
         squarelotron.rotateRight(1);
-        Assert.assertEquals(expectedResult, squarelotron.squarelotron);
+        Assert.assertEquals(expectedResult, squarelotron.getSquarelotron());
     }
 
+    /**
+     * Tests rotateRing method dimension 4 rotates 3.
+     */
     @Test
     public void testRotateRight3() {
-        int[][] expectedResult = {
+        final int[][] expectedResult = {
                 {4, 8, 12, 16},
                 {3, 7, 11, 15},
                 {2, 6, 10, 14},
                 {1, 5, 9, 13}
         };
-        Squarelotron squarelotron = new Squarelotron(4);
-        squarelotron.rotateRight(3);
-        Assert.assertEquals(expectedResult, squarelotron.squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FOUR);
+        squarelotron.rotateRight(THREE);
+        Assert.assertEquals(expectedResult, squarelotron.getSquarelotron());
     }
 
+    /**
+     * Tests rotateRing method dimension 3 rotates -1.
+     */
     @Test
     public void testRotateRight3Dimension3() {
-        int[][] expectedResult = {
+        final int[][] expectedResult = {
                 {3, 6, 9},
                 {2, 5, 8},
                 {1, 4, 7}
         };
-        Squarelotron squarelotron = new Squarelotron(3);
+        Squarelotron squarelotron = new Squarelotron(THREE);
         squarelotron.rotateRight(-1);
-        Assert.assertEquals(expectedResult, squarelotron.squarelotron);
+        Assert.assertEquals(expectedResult, squarelotron.getSquarelotron());
     }
 
+    /**
+     * Tests rotateRing method dimension 3 rotates 4.
+     */
     @Test
     public void testRotate4() {
         final int[][] expectedResult = {
@@ -135,15 +176,29 @@ public class SquarelotronTest {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        Squarelotron squarelotron = new Squarelotron(3);
-        squarelotron.rotateRight(4);
-        Assert.assertEquals(expectedResult, squarelotron.squarelotron);
+        Squarelotron squarelotron = new Squarelotron(THREE);
+        squarelotron.rotateRight(FOUR);
+        Assert.assertEquals(expectedResult, squarelotron.getSquarelotron());
     }
 
+    /**
+     * Tests rotateRing method dimension 3 rotates -4.
+     */
     @Test
-    public void oddNTest() {
+    public void testRotate4Negative() {
+        final int[][] expectedResult = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        Squarelotron squarelotron = new Squarelotron(THREE);
+        squarelotron.rotateRight(-EIGHT);
+        Assert.assertEquals(expectedResult, squarelotron.getSquarelotron());
     }
 
+    /**
+     * Tests MainDiagonalFlip method dimension 5 ring 2.
+     */
     @Test
     public void testMainDiagonalFlipDimensionFiveRingTwo() {
         final int[][] expectedResult = {
@@ -153,8 +208,34 @@ public class SquarelotronTest {
                 {16, 9, 14, 19, 20},
                 {21, 22, 23, 24, 25}
         };
-        Squarelotron squarelotron = new Squarelotron(5);
-//        squarelotron.mainDiagonalFlip(2).printSquarelotron();
-        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(2).squarelotron);
+        Squarelotron squarelotron = new Squarelotron(FIVE);
+        Assert.assertEquals(expectedResult, squarelotron.mainDiagonalFlip(2).getSquarelotron());
+    }
+
+    /**
+     * Tests belongs to ring out of borders.
+     */
+    @Test
+    public void testBelongsToTheRingOutOfBorders() {
+        Squarelotron squarelotron = new Squarelotron(1);
+        Assert.assertFalse(squarelotron.belongsToTheRing(-1, 1, 2));
+    }
+
+    /**
+     * Tests belongs to ring out of borders.
+     */
+    @Test
+    public void testBelongsToTheRingOutOfBordersX() {
+        Squarelotron squarelotron = new Squarelotron(FIVE);
+        Assert.assertFalse(squarelotron.belongsToTheRing(THREE, THREE, 2));
+    }
+
+    /**
+     * Tests belongs to matrix method.
+     */
+    @Test
+    public void testBelongsToTheMatrix() {
+        Squarelotron squarelotron = new Squarelotron(FIVE);
+        Assert.assertFalse(squarelotron.belongsToTheMatrix(FIVE, EIGHT, 2));
     }
 }
