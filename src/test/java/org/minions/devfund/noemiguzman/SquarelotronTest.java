170ebe29e258b70f0653f74e9d09953409a902b5
Noemi Guzman <noemi.guzo.ju@gmail.com>
2018-08-10 09:51:25 -0400

    HW2Squarelotron move variables into method

--- src/test/java/org/minions/devfund/noemiguzman/SquarelotronTest.java
+++ src/test/java/org/minions/devfund/noemiguzman/SquarelotronTest.java
@@ -13,17 +13,15 @@ public class SquarelotronTest {
     private Squarelotron s;
     private Squarelotron s4;
     private Squarelotron s5;
-    // different Size
-    static final int SIZE3 = 3;
-    static final int SIZE4 = 4;
-    static final int SIZE5 = 5;
+
     /**
      * Method to test size.
      */
     @Test
     public void testSquarelotronConstructorSize() {
-        s4 = new Squarelotron(SIZE4);
-        assertEquals(SIZE4, s4.getSize());
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
+        assertEquals(size4, s4.getSize());
     }
     /**
      * Method to test Constructor Element.
@@ -39,7 +37,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testSquarelotronConstructorElements() {
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         final int[][] expected = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         assertArrayEquals(expected, s4.getSquarelotron());
     }
@@ -48,7 +47,8 @@ public class SquarelotronTest {
      */
     @Test
     public  void testRotateRight1() {
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         s4.rotateRight(1);
         final int[][] expected = {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -58,7 +58,8 @@ public class SquarelotronTest {
      */
     @Test
     public  void testRotateRightNegative1() {
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         s4.rotateRight(-1);
         final int[][] expected = {{4, 8, 12, 16}, {3, 7, 11, 15}, {2, 6, 10, 14}, {1, 5, 9, 13}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -70,7 +71,8 @@ public class SquarelotronTest {
     @Test
     public void testRotateRight2() {
         final int numberOfTurns2 = 2;
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         s4.rotateRight(numberOfTurns2);
         final int[][] expected = {{16, 15, 14, 13}, {12, 11, 10, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -82,7 +84,8 @@ public class SquarelotronTest {
     @Test
     public void testRotateRightNegative2() {
         final int numberOfTurns2 = 2;
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         s4.rotateRight(-numberOfTurns2);
         final int[][] expected = {{16, 15, 14, 13}, {12, 11, 10, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -93,7 +96,8 @@ public class SquarelotronTest {
     @Test
     public void testRotateRight3() {
         final int numberOfTurns3 = 3;
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         s4.rotateRight(numberOfTurns3);
         final int[][] expected = {{4, 8, 12, 16}, {3, 7, 11, 15}, {2, 6, 10, 14}, {1, 5, 9, 13}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -104,7 +108,9 @@ public class SquarelotronTest {
     @Test
     public void testRotateRightNegative3() {
         final int numberOfTurns3 = 3;
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
+
         s4.rotateRight(-numberOfTurns3);
         final int[][] expected = {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -114,8 +120,9 @@ public class SquarelotronTest {
      */
     @Test
     public void testRotateRight4() {
-        s4 = new Squarelotron(SIZE4);
-        s4.rotateRight(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
+        s4.rotateRight(size4);
         final int[][] expected = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         assertArrayEquals(expected, s4.getSquarelotron());
     }
@@ -124,8 +131,10 @@ public class SquarelotronTest {
      */
     @Test
     public void testRotateRight5() {
-        s4 = new Squarelotron(SIZE4);
-        s4.rotateRight(SIZE5);
+        final int size4 = 4;
+        final int size5 = 5;
+        s4 = new Squarelotron(size4);
+        s4.rotateRight(size5);
         final int[][] expected = {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
         assertArrayEquals(expected, s4.getSquarelotron());
     }
@@ -134,8 +143,9 @@ public class SquarelotronTest {
      */
     @Test
     public void testRotateRight6() {
+        final int size4 = 4;
         final int numberOfTurns6 = 6;
-        s4 = new Squarelotron(SIZE4);
+        s4 = new Squarelotron(size4);
         s4.rotateRight(numberOfTurns6);
         final int[][] expected = {{16, 15, 14, 13}, {12, 11, 10, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
         assertArrayEquals(expected, s4.getSquarelotron());
@@ -145,7 +155,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testMainDiagonalFlipDimensionTwo() {
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         final int ring2 = 2;
         final int[][] expected = {{1, 2, 3, 4}, {5, 6, 10, 8}, {9, 7, 11, 12}, {13, 14, 15, 16}};
         Squarelotron temp5 = s4.mainDiagonalFlip(ring2);
@@ -156,7 +167,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testMainDiagonalFlipDimensionFiveRingThree() {
-        s4 = new Squarelotron(SIZE5);
+        final int size5 = 5;
+        s4 = new Squarelotron(size5);
         final int ring3 = 3;
         final int[][] expected = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20},
                 {21, 22, 23, 24, 25}};
@@ -168,7 +180,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testUpsideDownFlipDimensionFour() {
-        s4 = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s4 = new Squarelotron(size4);
         final int ring2 = 2;
         // 4 by 4, but flip inner ring
         final int[][] test4 = {{1, 2, 3, 4}, {5, 10, 11, 8}, {9, 6, 7, 12}, {13, 14, 15, 16}};
@@ -194,7 +207,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testUpsideDownFlipNewObject() {
-        s = new Squarelotron(SIZE4);
+        final int size4 = 4;
+        s = new Squarelotron(size4);
         Squarelotron temp = s.upsideDownFlip(1);
         final int[][] test = {{13, 14, 15, 16}, {9, 6, 7, 12}, {5, 10, 11, 8}, {1, 2, 3, 4}};
         assertArrayEquals(test, temp.getSquarelotron());
@@ -204,7 +218,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testUpsideDownFlipDimensionThree() {
-        s = new Squarelotron(SIZE3);
+        final int size3 = 3;
+        s = new Squarelotron(size3);
         Squarelotron temp = s.upsideDownFlip(1);
         final int[][] test = {{7, 8, 9}, {4, 5, 6}, {1, 2, 3}};
         assertArrayEquals(test, temp.getSquarelotron());
@@ -223,7 +238,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testMainDiagonalFlipNewObject() {
-        s = new Squarelotron(SIZE3);
+        final int size3 = 3;
+        s = new Squarelotron(size3);
         Squarelotron temp = s.mainDiagonalFlip(1);
         final int[][] test = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
         assertArrayEquals(test, temp.getSquarelotron());
@@ -233,7 +249,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testMainDiagonalFlipDimensionFiveRingOne() {
-        s5 = new Squarelotron(SIZE5);
+        final int size5 = 5;
+        s5 = new Squarelotron(size5);
         Squarelotron temp = s5.mainDiagonalFlip(1);
         final int[][] test = {{1, 6, 11, 16, 21}, {2, 7, 8, 9, 22}, {3, 12, 13, 14, 23}, {4, 17, 18, 19, 24},
                 {5, 10, 15, 20, 25}};
@@ -244,7 +261,8 @@ public class SquarelotronTest {
      */
     @Test
     public void testMainDiagonalFlipDimensionFiveRingTwo() {
-        s5 = new Squarelotron(SIZE5);
+        final int size5 = 5;
+        s5 = new Squarelotron(size5);
         Squarelotron temp = s5.mainDiagonalFlip(2);
         final int[][] test = {{1, 2, 3, 4, 5}, {6, 7, 12, 17, 10}, {11, 8, 13, 18, 15}, {16, 9, 14, 19, 20},
                 {21, 22, 23, 24, 25}};
