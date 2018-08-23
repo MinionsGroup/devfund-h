51b2925751aadaf46903855c76b91dc8ba9faee2
Noemi Guzman <noemi.guzo.ju@gmail.com>
2018-08-23 16:58:40 -0400

    HW4 adding messing unit test empty ship

--- src/test/java/org/minions/devfund/noemiguzman/battleship/OceanTest.java
+++ src/test/java/org/minions/devfund/noemiguzman/battleship/OceanTest.java
@@ -203,4 +203,83 @@ public class OceanTest {
 
         assertEquals(sb.toString(), ocean.toString());
     }
+    /**
+     * test to string.
+     */
+    @Test
+    public void testIsGameOver() {
+        final int place8 = 8;
+        Ocean ocean = new Ocean();
+        BattleShip ship = new BattleShip();
+        ship.placeShipAt(0, 0, true, ocean);
+        for (int j = 0; j < place8; j++) {
+            ocean.shootAt(0, j);
+        }
+
+        assertEquals(place8, ocean.getHitCount());
+        assertEquals(1, ocean.getShipsSunk());
+        assertEquals(place8, ocean.getShotsFired());
+        assertFalse(ocean.isGameOver());
+    }
+    /**
+     * test shoot at with ships vertical.
+     */
+    @Test
+    public void testShootAtWithShipVertical() {
+        Ocean ocean = new Ocean();
+        BattleShip ship = new BattleShip();
+        final int place8 = 8;
+        final int expectedShots = 10;
+        ship.placeShipAt(0, 0, false, ocean);
+        for (int j = 0; j < place8; j++) {
+            ocean.shootAt(0, j);
+        }
+        ocean.shootAt(0, 1);
+        ocean.shootAt(0, 2);
+
+        assertEquals(1, ocean.getHitCount());
+        assertEquals(0, ocean.getShipsSunk());
+        assertEquals(expectedShots, ocean.getShotsFired());
+
+    }
+    /**
+     * test to string vertical.
+     */
+    @Test
+    public void testToStringVertical() {
+        final int sizeOcean = 20;
+        final int place8 = 8;
+        Ocean ocean = new Ocean();
+        BattleShip ship = new BattleShip();
+        ship.placeShipAt(0, 0, false, ocean);
+        for (int j = 0; j < place8; j++) {
+            ocean.shootAt(0, j);
+        }
+
+        assertEquals(1, ocean.getHitCount());
+        assertEquals(0, ocean.getShipsSunk());
+        assertEquals(place8, ocean.getShotsFired());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ");
+        for (int i = 0; i < sizeOcean; i++) {
+            sb.append(String.format("%3d", i));
+        }
+        sb.append("\n");
+
+        for (int i = 0; i < sizeOcean; i++) {
+            sb.append(String.format("%2d ", i));
+            for (int j = 0; j < sizeOcean; j++) {
+                if (i == 0 && j < 1) {
+                    sb.append("S  ");
+                } else {
+                    sb.append(".  ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        assertEquals(sb.toString(), ocean.toString());
+
+    }
 }
