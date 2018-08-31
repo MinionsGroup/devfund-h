c428add7d8ab27a66f83486112de037f1216f316
RichardSeverich <richard.severich@gmail.com>
2018-08-31 13:46:18 -0700

    Richard/CircularPrimes

--- src/main/java/org/minions/devfund/richard/CircularPrimes.java
+++ src/main/java/org/minions/devfund/richard/CircularPrimes.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
  */
 public class CircularPrimes {
 
-
+    private static final int TEEN = 10;
     private ArrayList<Integer> circularPrimesList;
     private int range;
 
@@ -35,6 +35,65 @@ public class CircularPrimes {
      * @param number number.
      * @return boolean.
      */
+    private boolean isCircularPrimeWithoutString(final int number) {
+        return isPrime(number) && isPrime(this.reverseNumber(number));
+    }
+
+
+    /**
+     * @param number number.
+     * @return int.
+     */
+    int reverseNumber(final int number) {
+        int numberOfDigits = this.getNumberOfDigits(number);
+        if (numberOfDigits <= 1) {
+            return number;
+        }
+        int reverse = 0;
+        int[] arrayNumbers = this.getArrayOfNumbers(number);
+        int ind = numberOfDigits - 1;
+        int decimal = (int) Math.pow(TEEN, ind);
+        while (ind >= 0) {
+            reverse += arrayNumbers[ind] * decimal;
+            decimal /= TEEN;
+            ind--;
+        }
+        return reverse;
+    }
+
+    /**
+     * @param number number.
+     * @return Array of Numbers.
+     */
+    int[] getArrayOfNumbers(final int number) {
+        int numberOfDigits = getNumberOfDigits(number);
+        int temp = number;
+        int module;
+        int[] arrayNumbers = new int[numberOfDigits];
+        for (int i = numberOfDigits - 1; i >= 0; i--) {
+            module = temp % TEEN;
+            arrayNumbers[i] = module;
+            temp /= TEEN;
+        }
+        return arrayNumbers;
+    }
+
+    /**
+     * @param number number.
+     * @return int.
+     */
+    int getNumberOfDigits(final int number) {
+        if (number >= TEEN) {
+            return getNumberOfDigits(number / TEEN) + 1;
+        } else {
+            return 1;
+        }
+    }
+
+    /**
+     * @param number number.
+     * @return boolean.
+     */
     private boolean isPrime(final int number) {
         return IntStream.rangeClosed(2, number / 2).noneMatch(i -> number % i == 0);
     }
@@ -45,6 +104,17 @@ public class CircularPrimes {
     void fillListToCircularPrimes() {
         for (int i = 1; i <= range; i++) {
             if (isCircularPrime(i)) {
+                circularPrimesList.add(i);
+            }
+        }
+    }
+
+    /**
+     * Fill to List.
+     */
+    void fillListToCircularPrimesWithoutStrings() {
+        for (int i = 1; i <= range; i++) {
+            if (isCircularPrimeWithoutString(i)) {
                 circularPrimesList.add(i);
             }
         }
--- src/test/java/org/minions/devfund/richard/CircularPrimesTest.java
+++ src/test/java/org/minions/devfund/richard/CircularPrimesTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -16,11 +17,97 @@ public class CircularPrimesTest {
      * Test.
      */
     @Test
-    public void testCodeWars() {
+    public void testFillListToCircularPrimes() {
         final int num = 100;
         final List<Integer> expectedResult = Arrays.asList(1, 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97);
         CircularPrimes prime = new CircularPrimes(num);
         prime.fillListToCircularPrimes();
         assertEquals(expectedResult, prime.getCircularPrimesList());
     }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void testFillListToCircularPrimesWithoutStrings() {
+        final int num = 100;
+        final List<Integer> expectedResult = Arrays.asList(1, 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97);
+        CircularPrimes prime = new CircularPrimes(num);
+        prime.fillListToCircularPrimesWithoutStrings();
+        assertEquals(expectedResult, prime.getCircularPrimesList());
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void testGetLengthNumber() {
+        final int one = 1;
+        final int two = 2;
+        final int three = 3;
+        final int four = 4;
+        final int teen = 10;
+        final int ninetyNine = 99;
+        final int hundred = 100;
+        final int hundredOne = 101;
+        final int oneThousand = 1000;
+        CircularPrimes prime = new CircularPrimes(hundred);
+        assertEquals(one, prime.getNumberOfDigits(one));
+        assertEquals(two, prime.getNumberOfDigits(teen));
+        assertEquals(two, prime.getNumberOfDigits(ninetyNine));
+        assertEquals(three, prime.getNumberOfDigits(hundred));
+        assertEquals(three, prime.getNumberOfDigits(hundredOne));
+        assertEquals(four, prime.getNumberOfDigits(oneThousand));
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void testGetArrayOfNumbers() {
+        final int hundred = 100;
+        CircularPrimes prime = new CircularPrimes(hundred);
+        final int teen = 10;
+        final int[] arrayTeen = {1, 0};
+        assertArrayEquals(arrayTeen, prime.getArrayOfNumbers(teen));
+        final int eightyNine = 89;
+        final int[] arrayEightyNine = {8, 9};
+        assertArrayEquals(arrayEightyNine, prime.getArrayOfNumbers(eightyNine));
+        final int hundredOne = 101;
+        final int[] arrayHundredOne = {1, 0, 1};
+        assertArrayEquals(arrayHundredOne, prime.getArrayOfNumbers(hundredOne));
+        final int oneThousand = 1000;
+        final int[] arrayOneThousand = {1, 0, 0, 0};
+        assertArrayEquals(arrayOneThousand, prime.getArrayOfNumbers(oneThousand));
+
+    }
+
+
+    /**
+     * Test.
+     */
+    @Test
+    public void testReverseNumber() {
+        final int one = 1;
+        CircularPrimes prime = new CircularPrimes(one);
+        assertEquals(one, prime.reverseNumber(one));
+        final int two = 2;
+        assertEquals(two, prime.reverseNumber(two));
+        final int teen = 10;
+        assertEquals(one, prime.reverseNumber(teen));
+        final int eightyNine = 89;
+        final int ninetyEight = 98;
+        assertEquals(ninetyEight, prime.reverseNumber(eightyNine));
+        final int hundred = 100;
+        assertEquals(one, prime.reverseNumber(hundred));
+        final int hundredOne = 101;
+        assertEquals(hundredOne, prime.reverseNumber(hundredOne));
+        final int oneThousand = 1000;
+        assertEquals(one, prime.reverseNumber(oneThousand));
+        final int num = 1324;
+        final int expected = 4231;
+        assertEquals(expected, prime.reverseNumber(num));
+
+    }
+
 }
