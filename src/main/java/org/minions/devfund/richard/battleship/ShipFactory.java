1b42951633b997609eaa3e2335462b8f29f42172
Carledriss Zeiss <carledriss@gmail.com>
2018-09-30 09:46:12 -0700

    refactor code (#79)

--- src/main/java/org/minions/devfund/richard/battleship/ShipFactory.java
+++ src/main/java/org/minions/devfund/richard/battleship/ShipFactory.java
@@ -2,13 +2,14 @@ package org.minions.devfund.richard.battleship;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * Ship Factory.
  */
 final class ShipFactory {
-    private static final Map<String, ShipBuilder> SHIP_FACTORY = new HashMap<>();
 
+    private static final Map<String, Supplier<Ship>> SHIP_FACTORY = new HashMap<>();
     static {
         SHIP_FACTORY.put("battleship", BattleShip::new);
         SHIP_FACTORY.put("battlecruiser", BattleCruiser::new);
@@ -25,24 +26,12 @@ final class ShipFactory {
     }
 
     /**
-     * shipBuilder interface.
-     */
-    interface ShipBuilder {
-        /**
-         * Builds a new ship.
-         *
-         * @return {@link Ship}.
-         */
-        Ship buildShip();
-    }
-
-    /**
      * This method get the instance of specific class of ship.
      *
      * @param shipType string type of ship.
      * @return specific ship instance.
      */
     static Ship getShip(final String shipType) {
-        return SHIP_FACTORY.getOrDefault(shipType, EmptySea::new).buildShip();
+        return SHIP_FACTORY.getOrDefault(shipType, EmptySea::new).get();
     }
 }
